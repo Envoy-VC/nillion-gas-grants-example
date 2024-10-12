3e6340ff71dae9376daecaf724353bdb93a7161e
Vedant Chainani <vedantchainani1084@gmail.com>
2024-10-12 09:14:36 +0530

    chore: update nav

--- apps/www/src/components/navbar/index.tsx
+++ apps/www/src/components/navbar/index.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   return (
     <div className='h-[6dvh] w-full border'>
       <div className='mx-auto flex h-full max-w-screen-xl items-center justify-between px-4'>
-        <div className='text-xl font-semibold'>Web3 Starter</div>
+        <div className='text-xl font-semibold'>Nillion Fee Grants</div>
         <ConnectWallet />
       </div>
     </div>
